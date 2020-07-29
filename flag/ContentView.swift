2cf707144ed8da09c357d121291e21ee3001008c
twber <hctsai@umich.edu>
2020-07-30 00:56:22 +0800

    Initial Commit

---
+++ flag/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  flag
+//
+//  Created by Bernice TSAI on 2020/7/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
